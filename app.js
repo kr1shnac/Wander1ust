09403d3afd5d4e823e19560b583a7cb984f79747
Krishna <krishnacgowda10@gmail.com>
2025-07-28 15:47:02 +0530

    few changes

--- app.js
+++ app.js
@@ -64,9 +64,9 @@ const sessionOptions = {
   },
 };
 
-// app.get("/", (req, res) => {
-//   res.send("Hiiiiii root");
-// });
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 
 app.use(session(sessionOptions));
 app.use(flash()); //use flash before routes
